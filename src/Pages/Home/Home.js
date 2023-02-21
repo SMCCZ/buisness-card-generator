f9d6d3c0d9c24e7b88ebd6cac02911d6c64eb44e
SMCCZ <sabir.mondal@cloudcraftz.io>
2023-02-21 14:46:27 +0530

    Modification made for items alignment

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
-import './Home.css'
+import "./Home.css";
 import logo from "../../assets/logo.png";
+import scanner from "../../assets/scanner.png";
 const Home = () => {
   return (
-    <div className="HomeContainer">
-      <img src={logo} ></img>
-      <h1 className="Heading">
-      Upgrade your marketing strategy with interactive QR codes.
-      </h1>
-      <p>
-      Try our generator now!
-      </p>
+    <div className="Home">
+      <div className="HomeContainer">
+        <img src={logo}></img>
+        <h1 className="Heading">
+          Upgrade your marketing strategy with interactive QR codes.
+        </h1>
+        <p>Try our generator now!</p>
+      </div>
+      <div className="ScannerImage">
+      <img width={"120px"} height={"120px"} src={scanner} alt="scanner"></img>
+      </div>
     </div>
   );
 };
